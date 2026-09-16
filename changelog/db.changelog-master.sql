9cb45f425cf3062b11926435df7c5c0eccf5aefb
Luis PARAGUAY <enrique.par90@gmail.com>
2026-09-15 19:25:39 -0500

    feat: add staging schema migration

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -5,3 +5,9 @@ CREATE SCHEMA IF NOT EXISTS workspace.bi_lab_lparaguaya90
 COMMENT 'Laboratorio 02 - BI y Big Data - UCV';
 
 --rollback DROP SCHEMA IF EXISTS workspace.bi_lab_lparaguaya90;
+
+--changeset estudiante:002
+CREATE SCHEMA IF NOT EXISTS workspace.bi_staging_bi_lab_lparaguaya90
+COMMENT 'Staging schema - BI and Big Data - Lab 03';
+
+--rollback DROP SCHEMA IF EXISTS workspace.bi_staging_bi_lab_lparaguaya90;
